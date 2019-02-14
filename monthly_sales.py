4ab02ab3b42c240b9a63065fec84fef83183ff84
kmalhotra13 <k9@sukarm.com>
2019-02-14 16:18:01 -0500

    Update monthly_sales.py

--- monthly_sales.py
+++ monthly_sales.py
@@ -77,7 +77,37 @@ for line in data["product"]:
 		numProducts = numProducts + 1
 print(Products)
 
+import itertools
+from operator import itemgetter
 
+teams = [
+    {"city": "New York", "name": "Yankees"},
+    {"city": "New York", "name": "Mets"},
+    {"city": "Boston", "name": "Red Sox"},
+    {"city": "New Haven", "name": "Ravens"}
+]
+
+sorted_teams = sorted(teams, key=itemgetter("city")) # sort by some attribute
+
+teams_by_city = itertools.groupby(sorted_teams, key=itemgetter("city")) # group by the sorted attribute
+#> <itertools.groupby object at 0x10339dc50>
+
+for city, teams in teams_by_city:
+    print("----------------------------")
+    print(city.upper() + ":")
+    for team in teams:
+        print("  + " + team["name"])
+
+#> ----------------------------
+#> BOSTON:
+#>   + Red Sox
+#> ----------------------------
+#> NEW HAVEN:
+#>   + Ravens
+#> ----------------------------
+#> NEW YORK:
+#>   + Yankees
+#>   + Mets
 
 
 
