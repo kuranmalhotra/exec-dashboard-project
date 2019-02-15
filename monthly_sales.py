c2144360ec4aa37e43a886853901d505090084ec
kmalhotra13 <k9@sukarm.com>
2019-02-14 23:32:44 -0500

    Update monthly_sales.py

--- monthly_sales.py
+++ monthly_sales.py
@@ -138,13 +138,15 @@ print("Visualizing the data...")
 print(line)
 
 print(type(products))
-objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
-print(type(objects))
-y_pos = np.arange(len(objects))
+products_tuple = tuple(products)
+print(type(products_tuple))
+#objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+print(type(products_tuple))
+y_pos = np.arange(len(products_tuple))
 performance = [10,8,6,4,2,1]
  
 plt.barh(y_pos, performance, align='center', alpha=0.5)
-plt.yticks(y_pos, objects)
+plt.yticks(y_pos, products_tuple)
 plt.xlabel('Usage')
 plt.title('Programming language usage')
  
