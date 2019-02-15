7165778552b4c9208df06e3a3d9d89f3fa0a9fb5
kmalhotra13 <k9@sukarm.com>
2019-02-14 23:21:45 -0500

    Python MatPlotLib update issue
    had to reinstall matplotlib and the python framework because for somereason pyplot was not compatible with my version of python

--- monthly_sales.py
+++ monthly_sales.py
@@ -7,7 +7,9 @@
 
 # TODO: import some modules and/or packages here
 
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pds
 import statistics as st
 import tkinter
@@ -135,7 +137,16 @@ print(line)
 print("Visualizing the data...")
 print(line)
 
-
+objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+y_pos = np.arange(len(objects))
+performance = [10,8,6,4,2,1]
+ 
+plt.barh(y_pos, performance, align='center', alpha=0.5)
+plt.yticks(y_pos, objects)
+plt.xlabel('Usage')
+plt.title('Programming language usage')
+ 
+plt.show()
 
 
 
