9de487594fd0c24b7af2d845337baf58b19678be
kmalhotra13 <k9@sukarm.com>
2019-05-02 18:47:48 -0400

    Create requirements.txt

---
+++ test/monthly_sales_test.py
@@ -0,0 +1,13 @@
+# Continuous testing and integration of monthly_sales.py
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import pandas as pds
+import statistics as st
+import tkinter
+from tkinter import filedialog
+import os
+import datetime as dt
+
